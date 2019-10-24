d8012eb4b00731231fcab78b356a516bda96bc16
Noble Obioma <nobioma1@gmail.com>
2019-10-24 15:54:38 +0100

    Create actions to get rooms

--- src/actions/game.js
+++ src/actions/game.js
@@ -1,11 +1,15 @@
 import { axiosWithAuth } from '../helpers';
 
 export const START_COMMAND = 'START_COMMAND';
+
 export const INITIALIZE = 'INITIALIZE';
 export const INITIALIZE_FAILURE = 'INITIALIZE_FAILURE';
 
 export const MOVE = 'MOVE';
 export const MOVE_FAILURE = 'MOVE_FAILURE';
+
+export const GET_ROOMS = 'GET_ROOMS';
+export const GET_ROOMS_FAILURE = 'GET_ROOMS_FAILURE';
 
 export const initialize = () => dispatch => {
   dispatch({ type: START_COMMAND });
@@ -15,7 +19,7 @@ export const initialize = () => dispatch => {
       dispatch({ type: INITIALIZE, payload: res.data });
     })
     .catch(err => {
-      dispatch({ type: INITIALIZE_FAILURE, payload: err.response.data });
+      dispatch({ type: INITIALIZE_FAILURE, payload: 'error starting up game' });
     });
 };
 
@@ -28,5 +32,19 @@ export const move = direction => dispatch => {
     })
     .catch(err => {
       dispatch({ type: MOVE_FAILURE, payload: err.response.data });
+    });
+};
+
+export const getRooms = () => dispatch => {
+  return axiosWithAuth()
+    .get('/api/adv/rooms/')
+    .then(res => {
+      dispatch({ type: GET_ROOMS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ROOMS_FAILURE,
+        payload: 'error loading game rooms',
+      });
     });
 };
