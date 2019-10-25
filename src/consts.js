c99c83f805bec8b6e3bebf67ba62e01994cbceea
Noble Obioma <nobioma1@gmail.com>
2019-10-25 01:21:59 +0100

    Add consts

--- src/consts.js
+++ src/consts.js
@@ -1,1 +1,3 @@
 export const API_HOST = process.env.REACT_APP_API_HOST;
+export const PUSHER_KEY = process.env.REACT_APP_PUSHER_KEY;
+export const PUSHER_CLUSTER = process.env.REACT_APP_PUSHER_CLUSTER;
